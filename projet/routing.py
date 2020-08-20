4799768f6c13fc374dc46550917b1f1eec4e4e72
Yao Josué <jyao2000@yahoo.fr>
2020-08-20 07:55:41 +0000

    configure import for routing.py

--- projet/routing.py
+++ projet/routing.py
@@ -1,5 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+from django.conf.urls import url
+
+from chat.consumers import ChatChonsumer
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+
 })
