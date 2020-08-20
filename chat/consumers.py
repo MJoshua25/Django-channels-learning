9bb05496d26ac2d9ff0d6851b62a540a7b42e8be
Yao Josué <jyao2000@yahoo.fr>
2020-08-20 08:30:13 +0000

    first backend message send

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,6 +10,15 @@ from .models import Thread, ChatMessage
 class ChatChonsumer(AsyncConsumer):
 	async def websocket_connect(self, event):
 		print("connected", event)
+		await self.send({
+			'type': "websocket.accept"
+		})
+
+		await asyncio.sleep(10)
+		await self.send({
+			'type': "websocket.send",
+			'text': 'hello world'
+		})
 
 	async def websocket_receive(self, event):
 		print("received", event)
